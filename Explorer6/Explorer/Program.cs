5852eceaf54b0b589e0541bb0ae6db543b59ca6e
Fenix374 <isip_s.g.lysenko@mpt.ru>
2023-11-09 22:29:11 +0300

    repos

---
+++ Explorer6/Explorer/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Xml.Serialization;
+using System.Text;
+
+public class Figure
+{
+    public string Name { get; set; }
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Figure() { }
+
+    public Figure(string name, double width, double height)
+    {
+        Name = name;
+        Width = width;
+        Height = height;
+    }
+}
+
+public class FileHandler
+{
+    private string _filePath;
+
+    public FileHandler(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    private Figure DeserializeJson(string content)
+    {
+        return JsonConvert.DeserializeObject<Figure>(content);
+    }
+
+    private Figure DeserializeXml(string content)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(Figure));
+        using (TextReader reader = new StringReader(content))
+        {
+            return (Figure)serializer.Deserialize(reader);
+        }
+    }
+
+    public Figure Load()
+    {
+        string content = File.ReadAllText(_filePath);
+        string extension = Path.GetExtension(_filePath);
+
+        switch (extension)
+        {
+            case ".json":
+                return DeserializeJson(content);
+            case ".xml":
+                return DeserializeXml(content);
+            default:
+                return new Figure(content, 0, 0);
+        }
+    }
+
+    private string SerializeJson(Figure figure)
+    {
+        return JsonConvert.SerializeObject(figure);
+    }
+
+    private string SerializeXml(Figure figure)
+    {
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(Figure));
+        using (StringWriter textWriter = new StringWriter())
+        {
+            xmlSerializer.Serialize(textWriter, figure);
+            return textWriter.ToString();
+        }
+    }
+
+    public void Save(Figure figure, string newExtension)
+    {
+        string content;
+        string newFilePath = Path.ChangeExtension(_filePath, newExtension);
+
+        switch (newExtension)
+        {
+            case ".json":
+                content = SerializeJson(figure);
+                break;
+            case ".xml":
+                content = SerializeXml(figure);
+                break;
+            default:
+                content = figure.Name;
+                break;
+        }
+
+        File.WriteAllText(newFilePath, content);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите путь к файлу:");
+        string filePath = Console.ReadLine();
+
+        FileHandler fileHandler = new FileHandler(filePath);
+        Figure figure = fileHandler.Load();
+
+        Console.WriteLine($"Name: {figure.Name}");
+        Console.WriteLine($"Width: {figure.Width}");
+        Console.WriteLine($"Height: {figure.Height}");
+
+        Console.WriteLine("Нажмите F1 для сохранения файла, Esc для выхода.");
+
+        while (true)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            if (keyInfo.Key == ConsoleKey.F1)
+            {
+                Console.WriteLine("Введите новое расширение файла (.json, .xml, .txt):");
+                string newExtension = Console.ReadLine();
+                fileHandler.Save(figure, newExtension);
+                Console.WriteLine($"Файл сохранен как {Path.GetFileName(filePath)}{newExtension}.");
+            }
+            else if (keyInfo.Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+        }
+    }
+}
---
+++ explorer7/explorer2/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+public class FileManager
+{
+    private static string currentDirectory;
+
+    public static void Start()
+    {
+        Console.WriteLine("Выберите диск:");
+
+        DriveInfo[] drives = DriveInfo.GetDrives();
+        for (int i = 0; i < drives.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {drives[i].Name}");
+        }
+
+        int selectedDriveIndex = ReadIntegerInput("Введите номер диска:");
+        if (selectedDriveIndex < 1 || selectedDriveIndex > drives.Length)
+        {
+            Console.WriteLine("Неправильный номер диска.");
+            return;
+        }
+
+        currentDirectory = drives[selectedDriveIndex - 1].RootDirectory.FullName;
+        ShowDirectoryContents();
+    }
+
+    private static void ShowDirectoryContents()
+    {
+        int currentItemIndex = 0;
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine($"Текущая директория: {currentDirectory}");
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(currentDirectory);
+            FileSystemInfo[] fileSystemInfos = directoryInfo.GetFileSystemInfos();
+
+            for (int i = 0; i < fileSystemInfos.Length; i++)
+            {
+                FileSystemInfo info = fileSystemInfos[i];
+                string itemName = info is DirectoryInfo ? $"[{info.Name}]" : info.Name;
+                string selectionMarker = i == currentItemIndex ? ">" : " ";
+                Console.WriteLine($"{selectionMarker} {i + 1}. {itemName}");
+            }
+
+            var key = Console.ReadKey(true).Key;
+
+            switch (key)
+            {
+                case ConsoleKey.UpArrow: 
+                    currentItemIndex = (currentItemIndex - 1 + fileSystemInfos.Length) % fileSystemInfos.Length;
+                    break;
+                case ConsoleKey.DownArrow: 
+                    currentItemIndex = (currentItemIndex + 1) % fileSystemInfos.Length;
+                    break;
+                case ConsoleKey.Enter:
+                    FileSystemInfo selectedInfo = fileSystemInfos[currentItemIndex];
+                    if (selectedInfo is DirectoryInfo)
+                    {
+                        currentDirectory = selectedInfo.FullName;
+                    }
+                    else if (selectedInfo is FileInfo)
+                    {
+                        OpenFile(selectedInfo.FullName);
+                    }
+                    break;
+                case ConsoleKey.Escape: 
+                    GoToParentDirectory();
+                    break;
+            }
+        }
+    }
+
+
+    private static void GoToParentDirectory()
+    {
+        DirectoryInfo currentInfo = new DirectoryInfo(currentDirectory);
+        if (currentInfo.Parent != null)
+        {
+            currentDirectory = currentInfo.Parent.FullName;
+        }
+    }
+
+    private static void OpenFile(string filePath)
+    {
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo(filePath) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Could not open file: {ex.Message}");
+            }
+        }
+
+        Console.WriteLine($"Запуск файла: {filePath}");
+    }
+
+    private static int ReadIntegerInput(string message)
+    {
+        int result;
+        while (true)
+        {
+            Console.Write($"{message} ");
+            if (int.TryParse(Console.ReadLine(), out result))
+            {
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ввод. Попробуйте еще раз.");
+            }
+        }
+    }
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        FileManager.Start();
+    }
+}
